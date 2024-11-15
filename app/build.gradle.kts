656db940a5a6ea40a3d735e1ac55ff84eb45ce7b
nao-f-lll <mopsyro@duck.com>
2024-11-15 12:38:55 +0100

    initial project structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+<<<<<<< HEAD
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 }
@@ -11,28 +12,54 @@ android {
         applicationId = "com.example.didaktikapp"
         minSdk = 24
         targetSdk = 34
+=======
+    id("com.android.application")
+    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 35
+
+    defaultConfig {
+        namespace = "com.example.didaktikapp"
+        applicationId = "com.example.didaktikapp"
+        minSdk = 21
+        targetSdk = 35
+>>>>>>> b4f5b0f (initial project structure)
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+<<<<<<< HEAD
         vectorDrawables {
             useSupportLibrary = true
         }
+=======
+>>>>>>> b4f5b0f (initial project structure)
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
+<<<<<<< HEAD
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
+=======
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+>>>>>>> b4f5b0f (initial project structure)
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+<<<<<<< HEAD
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -46,10 +73,24 @@ android {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
+=======
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.0"
+>>>>>>> b4f5b0f (initial project structure)
     }
 }
 
 dependencies {
+<<<<<<< HEAD
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -66,4 +107,21 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+=======
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.ui)
+    implementation(libs.material3)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.androidx.activity.compose.v170)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit.v113)
+    androidTestImplementation(libs.androidx.espresso.core.v340)
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.tooling)
+    implementation(libs.androidx.activity)
+>>>>>>> b4f5b0f (initial project structure)
 }
