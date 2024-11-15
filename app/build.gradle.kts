8151caeeb6056f787b70ea568d18f968982eed3c
nao-f-lll <mopsyro@duck.com>
2024-11-15 12:57:28 +0100

    solve merge conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,4 @@
 plugins {
-<<<<<<< HEAD
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-}
-
-android {
-    namespace = "com.example.didaktikapp"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.didaktikapp"
-        minSdk = 24
-        targetSdk = 34
-=======
     id("com.android.application")
     id("kotlin-android")
     id("org.jetbrains.kotlin.android")
@@ -26,54 +12,23 @@ android {
         applicationId = "com.example.didaktikapp"
         minSdk = 21
         targetSdk = 35
->>>>>>> b4f5b0f (initial project structure)
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-<<<<<<< HEAD
-        vectorDrawables {
-            useSupportLibrary = true
-        }
-=======
->>>>>>> b4f5b0f (initial project structure)
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-<<<<<<< HEAD
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-=======
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 
->>>>>>> b4f5b0f (initial project structure)
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-<<<<<<< HEAD
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-=======
 
     kotlinOptions {
         jvmTarget = "1.8"
@@ -85,29 +40,10 @@ android {
 
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.0"
->>>>>>> b4f5b0f (initial project structure)
     }
 }
 
 dependencies {
-<<<<<<< HEAD
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-=======
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.core)
     implementation(libs.androidx.appcompat)
@@ -123,5 +59,4 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     implementation(libs.androidx.activity)
->>>>>>> b4f5b0f (initial project structure)
 }
